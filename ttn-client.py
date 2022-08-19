8f2a7a09434de98cc990d01022bc9ab0fb6169f0
tamberg <tamberg@yaler.net>
2022-08-19 15:51:06 +0200

    Update.

--- ttn-client.py
+++ ttn-client.py
@@ -2,9 +2,13 @@ import paho.mqtt.client as mqtt
 import json
 import base64
 
+TTN_APP_ID = "..." # TODO, e.g. pi-lora-app
+TTN_DEVICE_ID = "..." # TODO, e.g. pi-lora-device
+TTN_APP_API_KEY = "..." # TODO, see TTN console API Keys
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("v3/TTN_APP_ID@ttn/devices/TTN_DEVICE_ID/up") # TODO
+    client.subscribe("v3/"+ TTN_APP_ID + "@ttn/devices/" + TTN_DEVICE_ID + "/up")
 
 def on_message(client, userdata, msg):
     #print(msg.topic + " " + str(msg.payload))
@@ -18,6 +22,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.username_pw_set("TTN_APP_ID@ttn", "TTN_APP_API_KEY") # TODO
+client.username_pw_set(TTN_APP_ID + "@ttn", TTN_APP_API_KEY)
 client.connect("eu1.cloud.thethings.network", 1883, 60)
 client.loop_forever()
