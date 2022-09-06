4cc91939deeec029252d2db5a0b43eedecfa6170
tamberg <tamberg@yaler.net>
2022-09-06 12:22:56 +0200

    Add file.

---
+++ rn2483-pir.py
@@ -0,0 +1,74 @@
+# Pi Zero W with RN2483 LoRaWAN via Grove UART port, plus RST
+# $ pip3 install gpiozero
+# $ pip3 install wiringpi
+# $ sudo nano /boot/config.txt
+# enable_uart=1
+# $ sudo raspi-config # > Interface Options > Serial Port > 
+# ... login shell ... No > ... serial port ... Yes
+# $ sudo reboot
+
+import syslog
+import time
+
+import wiringpi
+from gpiozero import MotionSensor
+
+SERIAL_PORT = "/dev/ttyS0" # Grove UART port
+SERIAL_BAUD = 57600 # Default rate from spec
+
+TTN_DEV_ADDR = "260B829E" # TODO
+TTN_NWK_S_KEY = "13F83181A27D6476836C833F56B1F2BC" # TODO
+TTN_APP_S_KEY = "25CB4FE34CF1A4DF79FDC460C819FCE1" # TODO
+
+def readResult(serial):
+    while not wiringpi.serialDataAvail(serial):
+        time.sleep(0.01);
+    while wiringpi.serialDataAvail(serial):
+        ch = wiringpi.serialGetchar(serial)
+        print(chr(ch), end="")
+
+def sendCmd(serial, cmd):
+    print(cmd)
+    wiringpi.serialPrintf(serial, cmd + "\r\n")
+    readResult(serial)
+
+def sendCmd2(serial, cmd):
+    sendCmd(serial, cmd)
+    readResult(serial)
+
+try:
+    pir = MotionSensor(22)
+
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(21, 1) # Use GPIO pin 2 as OUTPUT
+    wiringpi.digitalWrite(21, 1) # Set GPIO pin 2 to HIGH
+    wiringpi.digitalWrite(21, 0) # Set GPIO pin 2 to LOW
+    time.sleep(0.5)
+    wiringpi.digitalWrite(21, 1) # Set GPIO pin 2 to HIGH
+
+    serial = wiringpi.serialOpen(SERIAL_PORT, SERIAL_BAUD)
+
+    sendCmd(serial, "sys get ver")
+    sendCmd(serial, "sys get hweui")
+    sendCmd(serial, "mac reset 868")
+    sendCmd(serial, "mac set nwkskey {0}".format(TTN_NWK_S_KEY))
+    sendCmd(serial, "mac set appskey {0}".format(TTN_APP_S_KEY))
+    sendCmd(serial, "mac set devaddr {0}".format(TTN_DEV_ADDR))
+    sendCmd(serial, "mac set adr off")
+    sendCmd(serial, "mac set ar off")
+    sendCmd(serial, "mac set pwridx 1")
+    sendCmd(serial, "mac set dr 5")
+    sendCmd(serial, "mac save")
+    sendCmd2(serial, "mac join abp")
+    time.sleep(1)
+
+    while True:
+        if pir.motion_detected:
+            print("Sending notification...")
+            data = "Motion detected!"
+            h = str((data.encode('utf-8').hex()))
+            sendCmd2(serial, "mac tx uncnf 1 {0}".format(h))
+            time.sleep(10) # should be 15 * 60 (= 15 min)
+
+except Exception as ex:
+    syslog.syslog(ex.message)
